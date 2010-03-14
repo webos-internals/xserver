145c33d64b6ed68e2aa9f803ca256d6d5b9ce5e0
Will Dietz <w@wdtz.org>
2010-03-13 19:38:18 -0600

    add 'noreset' flag to prevent X closing

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ KEYMAP_DIR=$APP_DIR/share/X11/xkb/keymap
 export PATH=$PATH:$APP_DIR/bin
 #Portrait orientation
 #(for landscape, use 480x320)
-Xsdl -nolisten tcp -retro -screen 320x480x24 :0 &
+Xsdl -noreset -nolisten tcp -retro -screen 320x480x24 :0 &
 sleep 1
 #DISPLAY=:0.0 vte &
 xkbcomp -R$KEYMAP_DIR palm-pre :0.0
