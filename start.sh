b9e8e701ae252f8f907c0bf8a2d880fc553803d7
Will Dietz <w@wdtz.org>
2010-03-13 18:48:11 -0600

    first pass at xlauncher.
    probably rather incomplete, WIP

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#Script to run vte on the palm-pre
+# Assumes Xsdl is on the PATH on the phone
+
+APP_DIR=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xlauncher
+KEYMAP_DIR=$APP_DIR/share/X11/xkb/keymap
+
+#Portrait orientation
+#(for landscape, use 480x320)
+Xsdl -nolisten tcp -retro -screen 320x480x24 :0 &
+sleep 1
+DISPLAY=:0.0 vte &
+xkbcomp -R$KEYMAP_DIR palm-pre :0.0
+wait
