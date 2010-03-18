7d36a1d2f3b2bb6c4c3339eae6f481c6d86a0d94
Will Dietz <w@wdtz.org>
2010-03-18 02:17:04 -0500

    just invoke the shared 'start x server' script

--- start.sh
+++ start.sh
@@ -3,17 +3,6 @@
 
 #This is where the X server lives
 APP_DIR=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xorg-server
-KEYMAP_DIR=$APP_DIR/share/X11/xkb/keymap
 
-export PATH=$APP_DIR/bin:$PATH
-#Portrait orientation
-Xsdl -noreset -nolisten tcp -retro -screen 320x480x24 :0 &
-#Landscape
-#Xsdl -noreset -nolisten tcp -retro -screen 480x320x24 :0 &
-
-sleep 1
-xkbcomp -R$KEYMAP_DIR palm-pre :0.0
-wait
-
-#XXX: Add code that runs ~/.xinitrc or /media/internal/xinitrc if it exists?
-
+#Start the x-server
+DISPLAY=:0.0 $APP_DIR/start.sh
