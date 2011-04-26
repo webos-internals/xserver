3be285f89821e389d852a7317d72d08c02c81ca3
Will Dietz <w@wdtz.org>
2011-04-26 03:47:34 -0500

    Change 'start.sh' to be in xserver

--- armv7/start.sh
+++ armv7/start.sh
@@ -2,7 +2,7 @@
 #Launches the x server
 
 #This is where the X server lives
-APP_DIR=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xorg-server
+APP_DIR=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xserver
 
 #Start the x-server
 DISPLAY=:0.0 $APP_DIR/start.sh
