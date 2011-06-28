7ca6c174b942473e5b9628b7efd2ea82a444138c
Will Dietz <w@wdtz.org>
2011-06-28 15:01:19 -0500

    Simplify the startup script... also cleanup the lock files.

--- armv7/start.sh
+++ armv7/start.sh
@@ -3,22 +3,7 @@
 
 export DISPLAY=:0.0
 
-#This is where the X server lives
-APP_DIR=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xserver
-KEYMAP_DIR=$APP_DIR/share/X11/xkb/keymap
+bin/Xsdl -noreset -nolisten tcp -retro $DISPLAY
 
-export PATH=$APP_DIR/bin:$PATH
-#Portrait orientation
-forker `which Xsdl` -noreset -nolisten tcp -retro -screen 320x480x24 $DISPLAY
-#Landscape
-#Xsdl -noreset -nolisten tcp -retro -screen 480x320x24 $DISPLAY &
-
-sleep 1
-xkbcomp -R$KEYMAP_DIR palm-prex $DISPLAY
-
-# Make the modifiers sticky!
-xkbset exp 60 =sticky =twokey =latchlock
-xkbset sticky -twokey latchlock
-
-#XXX: Add code that runs ~/.xinitrc or /media/internal/xinitrc if it exists?
-
+# cleanup!
+rm /tmp/.X* -rf
