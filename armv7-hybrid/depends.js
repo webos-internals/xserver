8e3d6f557259d21cc393a6627a8360885fb8132f
Will Dietz <w@wdtz.org>
2011-06-30 17:41:19 -0500

    (Automatic commit)

--- armv7-hybrid/depends.js
+++
@@ -1,4 +0,0 @@
-enyo.depends(
-	"app.js",
-    "app.css"
-);
--- armv7/set_keymap_later.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Launches the x server
-
-export DISPLAY=:0.0
-
-# forker `which bin/Xsdl` -noreset -nolisten tcp -retro $DISPLAY
-
-sleep 3
-xkbcomp -Rshare/X11/xkb/keymap palm-prex $DISPLAY
