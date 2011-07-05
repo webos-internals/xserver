a2940022bbb3210c273cebf2dbde05ec1c3f004f
Rod Whitby <rod@whitby.id.au>
2011-07-05 16:24:56 +0930

    Fixed basename to dirname in start.sh

--- armv7/start.sh
+++ armv7/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-APP_DIR=`basename $0`
+APP_DIR=`dirname $0`
 
 export PATH=${APP_DIR}/bin:${PATH}
 
@@ -8,6 +8,6 @@ export LD_LIBRARY_PATH=${APP_DIR}}/xlib:${LD_LIBRARY_PATH}
 
 ${APP_DIR}/bin/forker ${APP_DIR}/set_keymap_later.sh
 
-#exec strace -f -o test.log ${APP_DIR}/bin/Xsdl -retro -noreset :0.0 >& /tmp/x.log
+#exec strace -f -o test.log ${APP_DIR}/bin/Xsdl -retro -noreset :0.0 >& /tmp/xserver.log
 
 exec ${APP_DIR}/bin/Xsdl -retro -noreset :0.0 >& /tmp/xserver.log
