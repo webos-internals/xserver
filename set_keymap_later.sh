a91b4167c99019bb3bf79872fac7eea0fae4dede
Will Dietz <w@wdtz.org>
2011-07-15 13:53:41 -0500

    set_keymap_later.sh: Update to set the stickykey stuff via xkbset.

--- set_keymap_later.sh
+++ set_keymap_later.sh
@@ -7,3 +7,9 @@ export DISPLAY=:0.0
 sleep 3
 
 ${APP_DIR}/bin/xkbcomp -R${APP_DIR}/share/X11/xkb/keymap palm-prex ${DISPLAY}
+
+# Enable sticky-key goodness (for the palm-prex keybindings)
+sleep 1
+${APP_DIR}/bin/xkbset exp 60 =sticky =twokey =latchlock
+${APP_DIR}/bin/xkbset sticky -twokey latchlock
+
