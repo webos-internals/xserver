d3a3b5bdfdb33e0385f918a62c362d185cbbc2d1
Will Dietz <w@wdtz.org>
2011-08-09 03:18:18 -0500

    Dynamically determine which keymapping to use based on 'uname'.

--- set_keymap_later.sh
+++ set_keymap_later.sh
@@ -6,9 +6,17 @@ export DISPLAY=:0.0
 
 sleep 3
 
-${APP_DIR}/bin/xkbcomp -R${APP_DIR}/share/X11/xkb/keymap palm-prex ${DISPLAY}
+# Determine which keymapping to use...
+KEYMAPPING=palm-prex
+uname -a|grep tenderloin > /dev/null
+if [ "$?" -eq "0" ]; then
+  KEYMAPPING=touchpad
+fi
+
+${APP_DIR}/bin/xkbcomp -R${APP_DIR}/share/X11/xkb/keymap ${KEYMAPPING} ${DISPLAY}
 
 # Enable sticky-key goodness (for the palm-prex keybindings)
+# This is safe for the touchpad mappings.
 sleep 1
 ${APP_DIR}/bin/xkbset exp 60 =sticky =twokey =latchlock
 ${APP_DIR}/bin/xkbset sticky -twokey latchlock
