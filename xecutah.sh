d0d2888e137e5edcbbc85a0ecab9403972721155
Will Dietz <w@wdtz.org>
2011-08-23 22:10:14 -0500

    Add tweaks support for keyboard size into xecutah.sh

--- xecutah.sh
+++ xecutah.sh
@@ -8,4 +8,12 @@ export LD_LIBRARY_PATH=${APP_DIR}/xlib:${LD_LIBRARY_PATH}
 
 ${APP_DIR}/bin/forker ${APP_DIR}/set_keymap_later.sh
 
-${APP_DIR}/bin/Xsdl -retro -noreset :0.0 &> /tmp/xserver.log &
+RESPONSE=$(luna-send -f -n 1 palm://org.webosinternals.tweaks.prefs/get '{"owner":"org.webosinternals.xserver", "keys":["vkbtype"]}')
+
+export VKB="-vkb $(echo "$RESPONSE"| grep vkbtype|cut -d\" -f4|tr -cd 'a-z')"
+
+if [ "$VKB" == "-vkb " ]; then
+    export VKB="";
+fi
+
+${APP_DIR}/bin/Xsdl -retro -noreset $VKB :0.0&> /tmp/xserver.log &
